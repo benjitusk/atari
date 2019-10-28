c05751834e8890fa274a10fd2ac0343961ecad22
benjixinator <35320919+benjixinator@users.noreply.github.com>
2019-10-27 20:56:11 -0400

    Update atari.js

--- atari.js
+++ atari.js
@@ -1,18 +1,28 @@
-let totalBricks = 5;
+let totalBricks = 15;
 let bricksA = [];
+let ball;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  
-  for (let i = 0; i < totalBricks; i ++) {
-    bricksA.push(new Brick(i));
-  }
-  
+    createCanvas(windowWidth, windowHeight);
+
+    for (let i = 0; i < totalBricks; i++) {
+        bricksA.push(new Brick(i));
+    }
+    
+   ball = new Ball();
+
 }
 
 function draw() {
-  background(0);
-  for (let brick of bricksA) {
-    brick.show();
-  }
+    let padSize = 70;
+    
+    ball.show();
+    background(0);
+    for (let brick of bricksA) {
+        brick.show();
+    }
+    
+    stroke(255);
+    strokeWeight(10);
+    line(mouseX - padSize, height * 11 / 12, mouseX + padSize, height * 11 / 12);
 }
