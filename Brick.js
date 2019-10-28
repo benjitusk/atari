6e12d4c9f98e530867e9a4bb07aa1b1ae7972805
benjixinator <35320919+benjixinator@users.noreply.github.com>
2019-10-27 20:51:41 -0400

    Update Brick.js

--- Brick.js
+++ Brick.js
@@ -1,17 +1,21 @@
 class Brick {
 
-  constructor(x_) {
-  this.x = x_ *20;
-  this.y = 30;
-  this.damage = 0;
-  }
-  
-  show() {
-    rect(this.x, this.y, 20, 20, 20);
-  }
-  
-  update() {
-  
-  }
+    constructor(x_) {
+        this.x = (x_ + 0.5) * 55;
+        this.y = 30;
+        this.damage = 0;
+        this.c = color(255, random(170), random(170));
+    
+    }
 
-}
+    show() {
+        fill(this.c);
+        noStroke();
+        rect(this.x, this.y, 50, 30, 7);
+    }
+
+    update() {
+        
+    }
+
+} 
