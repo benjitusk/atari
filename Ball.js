eb45ca8eba215fb89f79fb8ab6a899300faf0831
benjixinator <35320919+benjixinator@users.noreply.github.com>
2019-10-27 20:42:44 -0400

    Update Ball.js

--- Ball.js
+++ Ball.js
@@ -1,23 +1,21 @@
-class Ball {
+class Brick {
 
-  constructor () {
-  this.x;
-  this.y;
-  this.dx;
-  this.dy;
-  }
-  
-  show() {
-  
-  }
-  
-  bounce() {
-  
-  }
-  
-  update() {
-  
-  }
-  
+    constructor(x_) {
+        this.x = x_ * 45;
+        this.y = 30;
+        this.damage = 0;
+        this.c = color(255, random(170), random(170));
+    
+    }
 
-}
+    show() {
+        fill(this.c);
+        noStroke();
+        rect(this.x, this.y, 40, 30, 7);
+    }
+
+    update() {
+        
+    }
+
+} 
