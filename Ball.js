a06f7950351c8297f2b4cfba9678692a5c1f2611
benjixinator <35320919+benjixinator@users.noreply.github.com>
2019-10-28 14:46:48 -0700

    fixed Ball.js

--- Ball.js
+++ Ball.js
@@ -1,21 +1,22 @@
 class Ball {
 
-    constructor(x_) {
-        this.x = x_ * 45;
-        this.y = 30;
-        this.damage = 0;
-        this.c = color(255, random(170), random(170));
+    constructor() {
+        this.x = width / 2;
+        this.y = height * 11 / 12;
+        this.dx = 0;
+        this.dy = 0;
     
     }
 
     show() {
         fill(this.c);
         noStroke();
-        rect(this.x, this.y, 40, 30, 7);
+        ellipse(this.x, this.y, 14);
     }
 
-    update() {
-        
+    move() {
+        this.x += this.dx;
+        this.y += this.dy;
     }
 
 } 
