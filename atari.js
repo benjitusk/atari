883903cd0b070e55f8d919c12701d3c56e3aff8c
benjixinator <35320919+benjixinator@users.noreply.github.com>
2019-10-27 20:04:57 -0400

    add brick setup and show

--- atari.js
+++ atari.js
@@ -1,7 +1,18 @@
+let totalBricks = 5;
+let bricksA = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  
+  for (let i = 0; i < totalBricks; i ++) {
+    bricksA.push(new Brick(i));
+  }
+  
 }
 
 function draw() {
   background(0);
+  for (let brick of bricksA) {
+    brick.show();
+  }
 }
