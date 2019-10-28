4c9563d232f76b6c572dbee9a8efafe3e45632b1
benjixinator <35320919+benjixinator@users.noreply.github.com>
2019-10-27 20:09:19 -0400

    add show()

--- Brick.js
+++ Brick.js
@@ -1,13 +1,13 @@
 class Brick {
 
-  constructor() {
-  this.x;
-  this.y;
-  this.damage;
+  constructor(x_) {
+  this.x = x_ *20;
+  this.y = 30;
+  this.damage = 0;
   }
   
   show() {
-  
+    rect(this.x, this.y, 20, 20, 20);
   }
   
   update() {
